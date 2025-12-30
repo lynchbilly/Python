703f5cd43e2baee7d210cf4f2e90931cc87671eb
Billy Lynch <87466682+lynchbilly@users.noreply.github.com>
2025-12-30 13:06:06 -0600

    Add files via upload

---
+++ CS50/Problem Sets/Problem Set 0/Making Faces/faces.py
@@ -0,0 +1,23 @@
+import sys
+def user_input():
+    # Prompt the user for input
+    user_input = input 'Say Hi or Bye'
+
+    # Print the output
+print(user_input)
+
+# Check the user input and respond accordingly
+ 
+if user_input == "Hello":
+    print("Hello :)")
+elif user_input == "Hi":
+    print("Hi there :)")
+elif user_input == "Bye": 
+     print("Goodbye :(")     
+elif user_input == "Goodbye":
+     print("See you later :(")   
+else:
+    print("I don't understand.")           
+
+    
+    
