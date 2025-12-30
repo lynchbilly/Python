5729b11f0a9101bcd84681cb6464261f39226f93
Billy Lynch <87466682+lynchbilly@users.noreply.github.com>
2025-12-30 13:02:43 -0600

    Add files via upload

---
+++ CS50/CS50/Problem Sets/Problem Set 0/Indoor Voice/Indoor Voice/indoor.py.py
@@ -0,0 +1,7 @@
+#takes user input
+user_input = input("Enter your name in all caps: ")
+
+# Convert the string to all lowercase letters
+lowercase_string = user_input.lower()
+#print the users input in lower case
+print("Please use your indoor voice",  lowercase_string)
